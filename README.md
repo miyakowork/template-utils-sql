ab48341a8ff8ab2b0d9a5257a5110cd6b31b9b16
miyakowork <765934806@qq.com>
2017-03-29 20:15:31 +0800

    #1.0.0初始导入

---
+++ README.md
@@ -0,0 +1,21 @@
+# template-utils-sql
+template-projects中的内置简单sql生成工具
+## 更新日志 ## 
+#### 1.0.0发布日志
+- 初始版本发布
+## 开始使用 ##
+> maven 坐标  最新版本号请至中央仓库查询 当前 ```1.0.0```（2017年3月29日）
+```xml
+<dependency>
+  <groupId>me.wuwenbin</groupId>
+  <artifactId>template-utils-sql</artifactId>
+  <version>${template-version}</version>
+</dependency>
+```
+## 要求 
+- jdk 1.7 以上
+- 配合 `template-modules-dao` 使用更佳
+
+## 文档
+- [入门指南](https://github.com/miyakowork/template-modules-dao/wiki/入门)
+- [主要方法](https://github.com/miyakowork/template-modules-dao/wiki/主要方法API)
---
+++ src/main/java/me/wuwenbin/sql/entrance/SQLFactory.java
@@ -0,0 +1,57 @@
+package me.wuwenbin.sql.entrance;
+
+
+import me.wuwenbin.sql.factory.SQLBeanBuilder;
+import me.wuwenbin.sql.factory.SQLStrBuilder;
+
+/**
+ * entrance for sql util
+ * Created by wuwenbin on 2017/1/12.
+ *
+ * @author wuwenbin
+ * @since 1.1.0
+ */
+public final class SQLFactory {
+
+    private volatile static SQLBeanBuilder sqlBeanBuilder;
+    private volatile static SQLStrBuilder sqlStrBuilder;
+
+    private SQLFactory() {
+    }
+
+    /**
+     * generate {@link SQLBeanBuilder} instance
+     *
+     * @param #clazz 对应实体类
+     * @return {@link SQLBeanBuilder}
+     */
+    public static SQLBeanBuilder builder(Class<?> clazz) {
+        if (sqlBeanBuilder == null) {
+            synchronized (SQLBeanBuilder.class) {
+                if (sqlBeanBuilder == null) {
+                    sqlBeanBuilder = new SQLBeanBuilder(clazz);
+                }
+            }
+        } else if (clazz != sqlBeanBuilder.getBeanClass()) {
+            sqlBeanBuilder = new SQLBeanBuilder(clazz);
+        }
+        return sqlBeanBuilder;
+    }
+
+    /**
+     * generate {@link SQLStrBuilder} instance
+     *
+     * @return {@link SQLStrBuilder}
+     */
+    public static SQLStrBuilder builder() {
+        if (sqlStrBuilder == null) {
+            synchronized (SQLStrBuilder.class) {
+                if (sqlStrBuilder == null) {
+                    sqlStrBuilder = new SQLStrBuilder();
+                }
+            }
+        }
+        return sqlStrBuilder;
+    }
+
+}
